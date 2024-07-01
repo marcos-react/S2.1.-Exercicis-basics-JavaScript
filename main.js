d4b4c7d069de45511deb22bb220c3df7e9ab01e2
evangelio7 <cristiano7@posteo.net>
2024-07-01 11:50:31 +0200

    Nivell 1 Exercici 2

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 // Nivell 1 Exercici 1 -  Arrows functions
 
 const add = (a,b) => a + b;
+
+// Nivell 1 Exercici 2 - Arrow function - randomNumber with out parameters and a seq 1 to 100
+
+
+const randomNumber = () => {
+
+    var random = Math.floor(Math.random() * 100) + 1;
+    console.log(random);
+
+}
